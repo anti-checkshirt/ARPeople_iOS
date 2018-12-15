7e045e7f7664813746ade83972e5081e4e3f9181
tomoki69386 <tomoki_69386@yahoo.co.jp>
2018-12-15 14:19:16 +0900

    Add ARSample

--- ARPeople_iOS/HomeViewController.swift
+++ ARPeople_iOS/HomeViewController.swift
@@ -13,11 +13,34 @@ import SceneKit
 class HomeViewController: UIViewController {
     
     @IBOutlet var sceneView: ARSCNView!
+    private var hogeView = SCNNode()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         setLayout()
+        let configuration = ARWorldTrackingConfiguration()
+        sceneView.session.run(configuration)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        sceneView.session.pause()
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        hogeView.removeFromParentNode()
+        let baseView = customView(frame: view.bounds)
+        guard let image = createImage(view: baseView) else { return }
+        let node = WebNode(image: image, panelColor: UIColor.green, width: 0.5)
+        
+        let position = SCNVector3(x: 0, y: 0, z: -1) // ノードの位置は、左右：0m 上下：0m　奥に100cm
+        if let camera = sceneView.pointOfView {
+            node.position = camera.convertPosition(position, to: nil) // カメラ位置からの偏差で求めた位置
+        }
+        sceneView.scene.rootNode.addChildNode(node) // 生成したノードをシーンに追加する
+        hogeView = node
     }
     
     private func setLayout() {
@@ -25,15 +48,16 @@ class HomeViewController: UIViewController {
         self.navigationController!.navigationBar.shadowImage = UIImage()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func createImage(view:UIView) -> UIImage? {
+        UIGraphicsBeginImageContextWithOptions(view.frame.size, false, 0)
+        guard let context = UIGraphicsGetCurrentContext() else {
+            return nil
+        }
+        view.isHidden = false // 画像を取得する間だけ表示
+        view.layer.render(in: context)
+        let image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        view.isHidden = true // 再び非表示
+        return image
     }
-    */
-
 }
