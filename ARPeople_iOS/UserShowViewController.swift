7698f37acc896d02b43255a6f5aa674bb8943f71
tomoki69386 <tomoki_69386@yahoo.co.jp>
2018-12-16 04:59:53 +0900

    Add UserShow

---
+++ ARPeople_iOS/UserShowViewController.swift
@@ -0,0 +1,28 @@
+//
+//  UserShowViewController.swift
+//  ARPeople_iOS
+//
+//  Created by 築山朋紀 on 2018/12/16.
+//  Copyright © 2018 tomoki. All rights reserved.
+//
+
+import UIKit
+
+class UserShowViewController: UIViewController {
+    
+    @IBOutlet private weak var nameLabel: UILabel!
+    @IBOutlet private weak var ageLabel: UILabel!
+    @IBOutlet private weak var emailLabel: UILabel!
+    @IBOutlet private weak var twitterLabel: UILabel!
+    @IBOutlet private weak var githubLabel: UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        nameLabel.text = "ともき"
+        ageLabel.text = "17歳"
+        emailLabel.text = "tomoki@tomoki.com"
+        twitterLabel.text = "tomoki_sun"
+        githubLabel.text = "github"
+    }
+}
