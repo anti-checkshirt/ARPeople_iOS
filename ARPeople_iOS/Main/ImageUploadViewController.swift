e6f19452224a06ce0507087cde2d887ae3b70343
tomoki69386 <tomoki_69386@yahoo.co.jp>
2018-12-16 03:41:57 +0900

    Create ImageUpload

---
+++ ARPeople_iOS/Main/ImageUploadViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ImageUploadViewController.swift
+//  ARPeople_iOS
+//
+//  Created by 築山朋紀 on 2018/12/16.
+//  Copyright © 2018 tomoki. All rights reserved.
+//
+
+import UIKit
+
+class ImageUploadViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
