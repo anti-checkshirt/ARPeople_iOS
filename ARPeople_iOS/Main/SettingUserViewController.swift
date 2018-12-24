0a6741faaffffa45e2ba6ef522dc766bcda396c6
tomoki69386 <tomoki_69386@yahoo.co.jp>
2018-12-24 17:19:22 +0900

    Add ViewとStoryboardの紐づけ

--- ARPeople_iOS/Main/SettingUserViewController.swift
+++ ARPeople_iOS/Main/SettingUserViewController.swift
@@ -9,9 +9,29 @@
 import UIKit
 
 class SettingUserViewController: UIViewController {
+    
+    @IBOutlet private weak var twitterInputField: UITextField!
+    @IBOutlet private weak var githubInputField: UITextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        twitterInputField.delegate = self
+        githubInputField.delegate = self
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+}
+
+extension SettingUserViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if textField == twitterInputField {
+            githubInputField.becomeFirstResponder()
+        } else {
+            githubInputField.resignFirstResponder()
+        }
+        return true
     }
 }
