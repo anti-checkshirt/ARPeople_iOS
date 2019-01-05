9dfdf7bfb9408397f187913454078d8a35cc9d68
tomoki69386 <tomoki_69386@yahoo.co.jp>
2019-01-05 17:20:48 +0900

    Create AgeInputField

--- ARPeople_iOS/Extension/Custom/InputField.swift
+++ ARPeople_iOS/Extension/Custom/InputField.swift
@@ -27,3 +27,61 @@ class InputField: UITextField {
         self.backgroundColor = AppColor.bgGlay
     }
 }
+
+extension UITextFieldDelegate {
+    func textFieldDoneTouch(_ textField: UITextField) {}
+    func textFieldCancelTouch(_ textField: UITextField) {}
+}
+
+class AgeInputField: InputField {
+    
+    let datepicker = UIDatePicker()
+    var dateFormat: String = "yyyy/MM/DD"
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        setUp()
+    }
+    
+    private func setUp() {
+        let timeZone = datepicker.timeZone
+        datepicker.timeZone = timeZone
+        datepicker.datePickerMode = UIDatePicker.Mode.date
+        datepicker.minimumDate = Date()
+        
+        let vi = UIView(frame: datepicker.bounds)
+        vi.backgroundColor = UIColor.white
+        vi.addSubview(datepicker)
+        self.inputView = vi
+        
+        let toolBar = UIToolbar()
+        toolBar.barStyle = UIBarStyle.default
+        toolBar.isTranslucent = true
+        toolBar.tintColor = UIColor.black
+        let doneButton   = UIBarButtonItem(title: "決定", style: UIBarButtonItem.Style.done, target: self, action: #selector(self.donePressed))
+        let cancelButton = UIBarButtonItem(title: "キャンセル", style: UIBarButtonItem.Style.plain, target: self, action: #selector(self.cancelPressed))
+        let spaceButton  = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.flexibleSpace, target: nil, action: nil)
+        toolBar.setItems([cancelButton, spaceButton, doneButton], animated: false)
+        toolBar.isUserInteractionEnabled = true
+        toolBar.sizeToFit()
+        self.inputAccessoryView = toolBar
+    }
+    
+    @objc func donePressed() {
+        delegate?.textFieldDoneTouch(self)
+        self.text = "\(self.format(date: datepicker.date))"
+    }
+    
+    @objc func cancelPressed() {
+        delegate?.textFieldCancelTouch(self)
+        self.text = ""
+    }
+    
+    func format(date: Date) -> String{
+        let dateformatter = DateFormatter()
+        dateformatter.dateFormat = dateFormat
+        let strDate = dateformatter.string(from: date)
+        return strDate
+    }
+}
