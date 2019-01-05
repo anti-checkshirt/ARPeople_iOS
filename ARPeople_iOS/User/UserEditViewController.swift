3a611f34512a5b3b44786b4e3c9debb8ad6a9387
tomoki69386 <tomoki_69386@yahoo.co.jp>
2019-01-05 16:43:47 +0900

    Add 画面外タップでキーボード閉じる

--- ARPeople_iOS/User/UserEditViewController.swift
+++ ARPeople_iOS/User/UserEditViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class UserEditViewController: BaseViewController {
     
+    @IBOutlet private weak var baseScrollView: UIScrollView!
     @IBOutlet private weak var headerImageView: UIImageView!
     @IBOutlet private weak var userImageView: UserImageView!
     @IBOutlet private weak var nameInputField: InputField!
@@ -18,9 +19,17 @@ class UserEditViewController: BaseViewController {
     @IBOutlet private weak var twitterInputField: InputField!
     @IBOutlet private weak var githubInputField: InputField!
     @IBOutlet private weak var ageInputField: InputField!
+    
+    private var txtActiveField: UITextField?
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        nameInputField.delegate = self
+        jobInputField.delegate = self
+        twitterInputField.delegate = self
+        githubInputField.delegate = self
+        ageInputField.delegate = self
 
         headerImageView.setImage(url: "https://pbs.twimg.com/profile_banners/764414957920276480/1543038909/1500x500")
         userImageView.setImage(url: "https://pbs.twimg.com/profile_images/1061520538386915329/ExNUPGbF_400x400.jpg")
@@ -31,8 +40,16 @@ class UserEditViewController: BaseViewController {
         navigationItem.setRightBarButton(barButton, animated: true)
         
         profileTextField.placeholder = "プロフィール文章"
+        
+        let tapGesture = UITapGestureRecognizer(
+            target: self,
+            action: #selector(self.tapped))
+        baseScrollView.addGestureRecognizer(tapGesture)
     }
-
+    
+    @objc private func tapped(_ sender: UITapGestureRecognizer) {
+        self.view.endEditing(true)
+    }
 }
 
 extension UserEditViewController: DoneBarButtonViewDelegate {
@@ -43,5 +60,59 @@ extension UserEditViewController: DoneBarButtonViewDelegate {
         case .setting, .back:
             break
         }
+    }
+}
+
+extension UserEditViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        switch textField {
+        case nameInputField:
+            jobInputField.becomeFirstResponder()
+        case jobInputField:
+            profileTextField.becomeFirstResponder()
+        case profileTextField:
+            twitterInputField.resignFirstResponder()
+        case twitterInputField:
+            githubInputField.resignFirstResponder()
+        case githubInputField:
+            ageInputField.resignFirstResponder()
+        default:
+            textField.resignFirstResponder()
+        }
+        return true
+    }
+    
+    func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
+        txtActiveField = nil
+        return true
+    }
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        txtActiveField = textField
+    }
+    
+    private func configureObserver() {
+        let notificationCenter = NotificationCenter.default
+        notificationCenter.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardDidHideNotification, object: nil)
+        notificationCenter.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardDidShowNotification, object: nil)
+    }
+    
+    @objc private func keyboardWillShow(notification: Notification) {
+        guard let keyboardHeight = notification.keyboardFrameEnd?.height else { return }
+        guard  let navHeight = self.navigationController?.navigationBar.frame.height else { return }
+        guard let txtActiveField = self.txtActiveField else { return }
+        let myBoundHeight = UIScreen.main.bounds.height
+        let statusBarHeight = UIApplication.shared.statusBarFrame.height
+        
+        let txtLimit = txtActiveField.frame.origin.y + txtActiveField.frame.height + statusBarHeight + navHeight
+        let kbdLimit = myBoundHeight - keyboardHeight
+        
+        if txtLimit >= kbdLimit {
+            baseScrollView.contentOffset.y -= txtLimit - kbdLimit
+        }
+    }
+    
+    @objc private func keyboardWillHide(notification: Notification) {
+        baseScrollView.contentOffset.y = 0
     }
 }
