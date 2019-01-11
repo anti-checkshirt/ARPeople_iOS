29c02ffe86ee960ccc7d1b1dc9c285e469d51297
tomoki69386 <tomoki_69386@yahoo.co.jp>
2019-01-12 06:42:53 +0900

    Add UserLevel

---
+++ ARPeople_iOS/Model/UserLevel.swift
@@ -0,0 +1,20 @@
+//
+//  UserLevel.swift
+//  ARPeople_iOS
+//
+//  Created by 築山朋紀 on 2019/01/12.
+//  Copyright © 2019 tomoki. All rights reserved.
+//
+
+import SwiftyUserDefaults
+
+enum UserLevel: Int, DefaultsSerializable {
+    /// 登録してない
+    case unregistered
+    
+    /// 顔写真を登録してない
+    case unlearned
+    
+    /// 登録済み
+    case registered
+}
