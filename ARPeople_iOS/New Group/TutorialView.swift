daaffd219c48e93aad8b560b796b3d3f8bc6fbe5
tomoki69386 <tomoki_69386@yahoo.co.jp>
2019-01-20 10:39:02 +0900

    Create TutorialView

---
+++ ARPeople_iOS/New Group/TutorialView.swift
@@ -0,0 +1,14 @@
+//
+//  TutorialView.swift
+//  ARPeople_iOS
+//
+//  Created by 築山朋紀 on 2019/01/20.
+//  Copyright © 2019 tomoki. All rights reserved.
+//
+
+import UIKit
+
+class TutorialView: UIView {
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+}
