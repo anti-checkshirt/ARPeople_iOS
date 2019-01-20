ea584edb479e9b4f1521fe0848f42508793b87e8
tomoki69386 <tomoki_69386@yahoo.co.jp>
2019-01-20 10:32:01 +0900

    Update

---
+++ ARPeople_iOS/New Group/TutorialViewController.swift
@@ -0,0 +1,35 @@
+//
+//  TutorialViewController.swift
+//  ARPeople_iOS
+//
+//  Created by 築山朋紀 on 2019/01/20.
+//  Copyright © 2019 tomoki. All rights reserved.
+//
+
+import UIKit
+
+class TutorialViewController: UIViewController {
+    
+    @IBOutlet private weak var baseScrollView: UIScrollView!
+    @IBOutlet private weak var baseView: UIView!
+    @IBOutlet private weak var pageControl: UIPageControl!
+    @IBOutlet private weak var closeButton: UIButton!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
